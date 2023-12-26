2256a82c7bf39f3e98baacc81f23e34f9fb7ce9b
sohaib5602 <sohaibasif.5602@gmail.com>
2023-12-27 01:02:03 +0500

    updated navmenu

---
+++ User_interface/wwwroot/js/custom.js
@@ -0,0 +1,15 @@
+﻿// custom.js
+window.closeSidebarOnClickOutside = () => {
+    document.addEventListener('click', function (event) {
+        const sidebar = document.getElementById('sidebar');
+        const menuIcon = document.querySelector('.menu-icon');
+
+        if (!sidebar.contains(event.target) && event.target !== menuIcon) {
+            // Close the sidebar if it's open
+            const sidebarClass = sidebar.getAttribute('class');
+            if (sidebarClass && sidebarClass.includes('active')) {
+                sidebar.classList.remove('active');
+            }
+        }
+    });
+};
